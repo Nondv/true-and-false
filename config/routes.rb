8dc67d0834cd05e549aed4bd460fb539d0c07be0
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-23 00:13:30 +0300

    rails generate scaffold attempts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :attempts
   # devise_for :users
   resources :statements
   root to: 'statements#index'
---
+++ test/controllers/attempts_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class AttemptsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @attempt = attempts(:one)
+  end
+
+  test "should get index" do
+    get attempts_url
+    assert_response :success
+  end
+
+  test "should create attempt" do
+    assert_difference('Attempt.count') do
+      post attempts_url, params: { attempt: {  } }
+    end
+
+    assert_response 201
+  end
+
+  test "should show attempt" do
+    get attempt_url(@attempt)
+    assert_response :success
+  end
+
+  test "should update attempt" do
+    patch attempt_url(@attempt), params: { attempt: {  } }
+    assert_response 200
+  end
+
+  test "should destroy attempt" do
+    assert_difference('Attempt.count', -1) do
+      delete attempt_url(@attempt)
+    end
+
+    assert_response 204
+  end
+end
