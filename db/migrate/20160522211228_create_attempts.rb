47599d510e13ba5f98d86d6db6e5d2c424b7e14f
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-23 00:23:18 +0300

    update migration create_attempts

--- db/migrate/20160522211228_create_attempts.rb
+++ db/migrate/20160522211228_create_attempts.rb
@@ -1,8 +1,14 @@
 class CreateAttempts < ActiveRecord::Migration[5.0]
   def change
     create_table :attempts do |t|
+      t.integer :user_id, null: false
+      t.integer :statement_id, null: false
+      t.boolean :success, null: false
 
       t.timestamps
     end
+
+    add_index :attempts, :user_id
+    add_index :attempts, :statement_id
   end
 end
--- test/models/attempt_test.rb
+++ test/models/attempt_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AttemptTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_attributes
+    model_attributes = Attempt.columns_hash.map { |name, col| [name.to_sym, col.type.to_sym] }.to_h
+    expected_attributes = { id: :integer,
+                            user_id: :integer,
+                            statement_id: :integer,
+                            success: :boolean,
+
+                            created_at: :datetime,
+                            updated_at: :datetime }
+
+    assert_equal(expected_attributes, model_attributes)
+  end
 end
