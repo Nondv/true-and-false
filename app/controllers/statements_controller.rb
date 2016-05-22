8d7b900595691fa73545c8fbef31a4414ecb29a2
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-22 12:53:18 +0300

    add callbacks to renderings

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -13,14 +13,14 @@ class StatementsController < ApplicationController
                  .first
 
     authorize @statement
-    render json: @statement
+    render json: @statement, callback: params[:callback]
   end
 
   alias index random
 
   # GET /statements/1
   def show
-    render json: @statement
+    render json: @statement, callback: params[:callback]
   end
 
   # POST /statements
@@ -29,18 +29,18 @@ class StatementsController < ApplicationController
     authorize @statement
 
     if @statement.save
-      render json: @statement, status: :created, location: @statement
+      render json: @statement, status: :created, location: @statement, callback: params[:callback]
     else
-      render json: @statement.errors, status: :unprocessable_entity
+      render json: @statement.errors, status: :unprocessable_entity, callback: params[:callback]
     end
   end
 
   # PATCH/PUT /statements/1
   def update
     if @statement.update(statement_params)
-      render json: @statement
+      render json: @statement, callback: params[:callback]
     else
-      render json: @statement.errors, status: :unprocessable_entity
+      render json: @statement.errors, status: :unprocessable_entity, callback: params[:callback]
     end
   end
 
