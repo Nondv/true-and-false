2af523d1c79542a2fc6e0237e7301a85ff612cff
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-24 02:35:31 +0300

    game controller + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  post '/answer/:id', to: 'game#make_a_guess'
+  root 'game#random_gamecard'
+
   resources :attempts
   # devise_for :users
   resources :statements
-  root to: 'statements#index'
 end
