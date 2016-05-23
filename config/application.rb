4bd5a9981359704d2a420ea764b1f2fff997997e
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-24 02:55:08 +0300

    bump version to 1.0.0

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module TrueAndFalse
-  VERSION = '0.2.0-game.controller'.freeze
+  VERSION = '1.0.0'.freeze
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
