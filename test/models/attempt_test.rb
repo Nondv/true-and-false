9f124c37b85ce8c9e35eb891c5ca98791a074792
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-23 03:00:22 +0300

    some Attempt model tests

--- test/models/attempt_test.rb
+++ test/models/attempt_test.rb
@@ -18,4 +18,40 @@ class AttemptTest < ActiveSupport::TestCase
     expected_amount = 5
     assert_equal(expected_amount, Attempt.count)
   end
+
+  test ':user presence validation' do
+    a = Attempt.new
+    assert_equal(false, a.validate)
+    assert_includes(a.errors.keys, :user)
+  end
+
+  test ':statement presence validation' do
+    a = Attempt.new
+    assert_equal(false, a.validate)
+    assert_includes(a.errors.keys, :statement)
+  end
+
+  test ':user existence validation' do
+    u = users(:plain_user)
+    user_id = u.id
+    a = Attempt.new(user_id: user_id)
+    assert_not_includes(a.errors.keys, :user) unless a.validate
+    u.delete
+
+    a = Attempt.new(user_id: user_id)
+    assert_equal(false, a.validate)
+    assert_includes(a.errors.keys, :user)
+  end
+
+  test ':statement existence validation' do
+    s = statements(:one)
+    a = Attempt.new(statement_id: s.id)
+
+    assert_not_includes(a.errors.keys, :statement) unless a.validate
+    s.destroy!
+
+    a = Attempt.new(statement_id: s.id)
+    assert_equal(false, a.validate)
+    assert_includes(a.errors.keys, :statement)
+  end
 end
