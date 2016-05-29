0474c031f9162a3fceb4a673545432405f393c55
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-29 19:47:17 +0300

    update tests for users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @admin = users(:admin)
+    @user = users(:plain_user)
+    @statement = statements(:one)
+  end
+
+  test 'should force auth' do
+    get users_url
+    assert_response 401
+  end
+
+  test 'should show me' do
+    [@admin, @user].each do |u|
+      get_as u, users_me_url
+      assert_response :success
+      assert_equal(json_response.symbolize_keys, u.as_json)
+    end
+  end
+
+  test 'should update me' do
+    update_as @user,
+              users_me_url,
+              params: { email: "update_#{@user.email}" }
+
+    assert_response :success
+    assert_equal("update_#{@user.email}", User.find(@user.id).email)
+  end
 end
