eecf76ccbb8e315e6bc01eae21c97a6b2982263d
Dmitriy Non <non.dmitriy@gmail.com>
2016-05-29 21:59:14 +0300

    update UsersController tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,7 +4,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @admin = users(:admin)
     @user = users(:plain_user)
-    @statement = statements(:one)
   end
 
   test 'should force auth' do
@@ -16,16 +15,65 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     [@admin, @user].each do |u|
       get_as u, users_me_url
       assert_response :success
-      assert_equal(json_response.symbolize_keys, u.as_json)
+      assert_equal(json_response, u.as_json(only: [:id, :email]))
     end
   end
 
-  test 'should update me' do
-    update_as @user,
-              users_me_url,
-              params: { email: "update_#{@user.email}" }
+  test 'should show users if I am admin' do
+    get_as @user, user_url(@user)
+    assert_response :forbidden
 
+    get_as @admin, user_url(@user)
     assert_response :success
-    assert_equal("update_#{@user.email}", User.find(@user.id).email)
+  end
+
+  test 'should get index if I am admin' do
+    get_as @user, users_url
+    assert_response :forbidden
+
+    get_as @admin, users_url
+    assert_response :success
+  end
+
+  test 'should update users if I am admin' do
+    new_email = "updated_#{@user.email}"
+    patch_as @user,
+             user_url(@user),
+             params: { email: new_email }
+    assert_response :forbidden
+
+    patch_as @admin,
+             user_url(@user),
+             params: { email: new_email }
+    assert_response 200
+    assert_equal(new_email, User.find(@user.id).email)
+  end
+
+  test 'should destroy users if I am admin' do
+    delete_as @user, user_url(@user)
+    assert_response :forbidden
+
+    delete_as @admin, user_url(@user)
+    assert_response 204
+    assert_nil User.find_by_id(@user.id)
+  end
+
+  test 'should create users if I am admin' do
+    attributes = { email: 'unexisted_user@test-test',
+                   password: '0000' }
+
+    post_as @user,
+            users_url,
+            params: { user: attributes }
+
+    assert_response :forbidden
+
+    assert_difference('User.count') do
+      post_as @admin,
+              users_url,
+              params: { user: attributes }
+    end
+
+    assert_response 201
   end
 end
